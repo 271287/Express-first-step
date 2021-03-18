6f4023d3927c6d32735ba7ea7fa59c5643f55aa0
Natalia Szymiczek <nat.szymiczek@gmail.com>
2021-03-18 20:30:36 +0100

    Add contact

--- server.js
+++ server.js
@@ -1,27 +1,35 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
-
 const app = express();
+
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
 app.get('/hello/:name', (req, res) => {
-  res.render('hello', {name: req.params.name });
+  res.render('hello', { name: req.params.name });
 });
 
 app.get('/about', (req, res) => {
   res.render('about');
 });
 
-app.get('/contact', (req, res) => {
-  res.render('contact');
+app.post('/contact/send-message', (req, res) => {
+  const { author, sender, title, message, file } = req.body;
+
+  if (author && sender && title && message && file) {
+    res.render('contact', { isSent: true, fileName: file });
+  } else {
+    res.render('contact', { isError: true });
+  }
 });
 
 app.get('/info', (req, res) => {
